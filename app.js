12264fb3fbe445654d47e12a3e3fee9222e99305
Paulorabeloo <pauloeduardorabelomiranda@gmail.com>
2025-02-04 05:55:47 -0300

    Atualização

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
-let titulo = document.querySelector('h1'); //selecionou a tag h1 no html
-titulo.innerHTML = 'Jogo do número secreto'
+//let titulo = document.querySelector('h1'); //selecionou a tag h1 no html
+//titulo.innerHTML = 'Jogo do número secreto'
 
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um número entre 1 e 10'
+//let paragrafo = document.querySelector('p');
+//paragrafo.innerHTML = 'Escolha um número entre 1 e 10'
+// os 2 jeitos funcionam mas boa prática é o de baixo:
+
+// vai gerar o número aleatorio e guardar na variável
+let numeroSecreto = gerarNumeroAleatorio();
+
+//função com parametros
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto
+}
+
+exibirTextoNaTela('h1', 'Jogo do número secreto');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+ 
+//função sem retorno e sem parametro
+function verificarChute() {
+    console.log(numeroSecreto);
+}
+
+//função com retorno
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * 10 + 1) //parseInt vai transformar em número inteiro
+}
