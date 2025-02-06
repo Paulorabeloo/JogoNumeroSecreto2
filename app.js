6f5a63d35a2ef3e632ff09e9d2898492843abc5e
Paulo Rabelo <paulo.rabelo@IBN.LOCAL>
2025-02-06 13:54:36 -0300

    Adição Tentativas

--- app.js
+++ app.js
@@ -7,6 +7,7 @@
 
 // vai gerar o número aleatorio e guardar na variável
 let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
 
 //função com parametros
 function exibirTextoNaTela(tag, texto) {
@@ -21,9 +22,12 @@ exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 function verificarChute() {
     // vai pegar o valor(value) da tag input no html e armazenar na variável chute
     let chute = document.querySelector('input').value;
+
     if (chute == numeroSecreto) {
         exibirTextoNaTela('h1', 'Acertou!');
-        exibirTextoNaTela('p', 'Parabéns, Você descobriu o número secreto');
+        let palavraTentativa = tentativas > 1? 'tentativas' : 'tentativa';
+        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
+        exibirTextoNaTela('p', mensagemTentativas);
     }
     else if (chute > numeroSecreto) {
         exibirTextoNaTela('p', 'O número secreto é menor');
@@ -31,6 +35,7 @@ function verificarChute() {
     else {
         exibirTextoNaTela('p', 'O número secreto é maior');
     }
+    tentativas++;
 }
 
 //função com retorno
